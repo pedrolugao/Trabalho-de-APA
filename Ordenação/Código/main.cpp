839bdef59e045011d32eedb1720620e7786ceae9
Pedro Lugão <pedrogasparetto@ymail.com>
2017-04-23 16:46:14 -0300

    Código Comentado

--- Ordenação/Código/main.cpp
+++ Ordenação/Código/main.cpp
@@ -5,10 +5,12 @@
 #include <math.h>
 #include<time.h>
 
-//REIMPLEMENTAR OS ALGORITMOS COM VETORES DESSA STRUCT E CONTAGEM DE INSTRUÇÕES
+//!Utilizamos essa biblioteca para fazer os testes de tempo
+//!Todos os códigos relativos a isso estarão comentados pois não funcionam em linux
+//#include<windows.h>
+
 typedef struct strItem{
     int chave;
-    //Informações (podemos adicionar mais coisa):
     int info;
 }Item;
 
@@ -106,7 +108,10 @@ Item* clona(Item* vet, int n) {
 }
 //
 
-//SELECTIONSORT
+//!SELECTIONSORT
+//A função recebe um vetor e seu tamanho
+//A cada iteração externa, busca o maior elemento com a iteração interna e coloca no fim do vetor
+//Ao fim da execução, o vetor estar ordenado
 void selectionSort(Item *vet, int n)
 {
     int iaux, i,j;
@@ -125,7 +130,11 @@ void selectionSort(Item *vet, int n)
 }
 //
 
-//BUBBLESORT
+//!BUBBLESORT
+//A função recebe um vetor e seu tamanho
+//A cada iteração interna, coloca o maior elemento de duas posições seguidas na maior posição entre eles.
+//A cada iteração externa, coloca o maior elemento do vetor na última posição
+//Ao fim de execução, o vetor estará ordenado
 void bubbleSort(Item *vet, int n){
     int i,j;
 
@@ -139,7 +148,11 @@ void bubbleSort(Item *vet, int n){
 }
 //
 
-//INSERTIONSORT
+//!INSERTIONSORT
+//A função recebe um vetor e seu tamanho
+//A cada iteração interna, encontra a posição de um elemento na parte ordenada do vetor (todos os elementos com posição menor que ele)
+//A cada iteração externa, aumenta em 1 ao tamanho da parte ordenada do vetor
+//Ao fim da execução, todo o vetor estará ordenado
 void insertionSort(Item * vet, int n){
     int i,j;
     Item aux;
@@ -158,7 +171,13 @@ void insertionSort(Item * vet, int n){
 }
 //
 
-//MERGESORT
+//! MERGESORT
+
+//Intercala: recebe um vetor, a posição de inicio de duas partições do vetor, e o seu tamanho
+//A função considera os elementos de L1 a L2 como um vetor, e os elementos de L2 a F como outro.
+//Assume-se que ambos os vetores estejam ordenados, e com isso a função os une de maneira ordenada em um novo vetor
+//Por fim, copia os elementos em ordem substituindo os elementos no vetor original
+//Ao fim de execução, os elementos de L1 a F do vetor estarão ordenados
 void intercala(Item v[],int L1, int L2, int F){
     int iL1 = L1, iL2 = L2, aux = 0;
     Item vAux[F-L1];
@@ -197,6 +216,9 @@ void intercala(Item v[],int L1, int L2, int F){
     }
 }
 
+//MergeSort: Função principal, recebe o vetor, a posição do primeiro elemento, e o seu tamanho.
+//Divide recursivamente os vetores em 2 até atingir tamanho unitário (um elemento = vetor ordenado), e a partir daí os une com a função de intercala;
+//Ao fim de execução, o vetor estará ordenado
 void mergeSort(Item v[],int inicio, int fim){ //fim = tamanho do vetor
     if(inicio<fim-1){
         int meio = (inicio + fim)/2 ;
@@ -207,7 +229,11 @@ void mergeSort(Item v[],int inicio, int fim){ //fim = tamanho do vetor
 }
 //
 
-//QUICKSORT
+//! QUICKSORT
+
+//Particao: recebe um vetor, o indice inicial e o final da parte do vetor a ser considerada
+//Seleciona o primeiro elemento à esquerda como pivô, e coloca os elementos menores que ele à sua esquerda, e os maiores à direita.
+//Retorna a posição final do pivô
 int particao(Item* vet, int esq, int dir){
     int indPivo = esq;
     esq++;
@@ -229,6 +255,9 @@ int particao(Item* vet, int esq, int dir){
     return dir;
 }
 
+//QuickSort: função principal, recebe um vetor, a primeira e a última posição do vetor.
+//Executa a partição com todos os elementos, mas rodando recursivamente em cada uma das partes dividida pelo pivô em cada execução
+//Ao fim da execução, o vetor estará ordenado
 void quickSort(Item *vet, int esq, int dir){
     if(esq<dir){
         int j = particao(vet,esq,dir);
@@ -238,7 +267,12 @@ void quickSort(Item *vet, int esq, int dir){
 }
 //
 
-//HEAPSORT
+//! HEAPSORT
+
+//Max_heapfy : recebe uma heap, uma posição de um elemento, e seu tamanho
+//Com isso, confere se o elemento na posição satisfaz a propriedade de max-heap (se seus filhos são menores)
+//Caso contrário, troca o elemento de lugar com o maior filho e reexecuta o código para ver se o elemento agora segue à propriedade de max-heap
+//Ao fim da execução, o elemento estará seguindo a propriedade de max-heap
 void max_heapfy(Item* vet, int pos, int tam){
     int maior = pos ,esq = 2*pos+1,dir = 2*pos+2;
     contadorComparacoes++;
@@ -258,6 +292,9 @@ void max_heapfy(Item* vet, int pos, int tam){
 
 }
 
+//Build_maxHeap : recebe uma heap e seu tamanho
+//Executa a função max_heapfy em todos os elementos que não são folha da heap (elementos até a metade do vetor)
+//Ao fim da execução, todos os elementos estarão seguindo a propriedade de max-heap, e a heap será uma max-heap
 void build_maxHeap(Item* vet, int tam){
     int i;
     for(i=tam/2;i>=0;i--){
@@ -265,6 +302,10 @@ void build_maxHeap(Item* vet, int tam){
     }
 }
 
+//HeapSort: função principal, recebe um vetor (heap) e seu tamanho
+//Consiste em pegar a raiz de uma max-heap, trocar com o ultimo elemento, e construir uma nova max-heap desconsiderando a raiz tirada;
+//A execução para quando não há mais elementos para criar uma max-heap
+//Ao fim da execução, o vetor estará ordenado
 void heapSort(Item *vet, int tam){
     int i;
     build_maxHeap(vet,tam);
@@ -275,15 +316,17 @@ void heapSort(Item *vet, int tam){
 }
 //
 
-//SORT
-//! Ordena um vetor recebendo o vetor, o tamanho e qual método deve ser usado, de acordo como enum sortMethod
-//! Retorna um bool que indica se o vetor resultado está ordenado ou não
+//! SORT
+//Ordena um vetor recebendo o vetor, o tamanho e qual método deve ser usado, de acordo como enum sortMethod
+//Retorna um bool que indica se o vetor resultado está ordenado ou não
 bool sortVet(Item* vet, int n, sortMethod method) {
     clock_t inicio, fim;
     contadorComparacoes = 0;
     contadorAtribuicoes = 0;
 
-    inicio = clock();
+    //!LARGE_INTEGER start, eNd, frequency;
+    //!QueryPerformanceCounter(&start);
+
 
 
     switch (method) {
@@ -307,11 +350,10 @@ bool sortVet(Item* vet, int n, sortMethod method) {
             break;
     }
 
-    fim = clock();
-    int tempo = fim - inicio;
-    tempo = tempo;
+    //!QueryPerformanceCounter(&eNd);
+    //!double tempo =  (double)(eNd.QuadPart - start.QuadPart)
 
-    std::cout << "Tempo consumido (em clocks): "<< tempo << std::endl;
+    //!std::cout << "Tempo consumido (em clocks): "<< tempo << std::endl;
 
     std::string stringNumComparacoes = std::string() + "N" + char(163) + "mero de compara" + char(135) + char(228) + "es: ";
     std::cout << stringNumComparacoes << contadorComparacoes << std::endl;
@@ -352,19 +394,7 @@ int main()
     sortMethod methods[AMOUNT_OF_METHODS] = {SELECTION, BUBBLE, INSERTION, MERGE, QUICK, HEAP};
     std::string methodsNames[AMOUNT_OF_METHODS] = {"Selection", "Bubble", "Insertion", "Merge", "Quick", "Heap"};
 
-    //PARA TESTAR 1 MÉTODO COM TODOS OS TAMANHOS DE 1 TIPO DE VETOR
-    /*
-    sortMethod method = INSERTION;
 
-    for (i = 0; i < AMOUNT_OF_SIZES; i++) {
-        Item* vet = clona(randomVets[i], sizes[i]);
-        std::cout << "Tamanho do vetor: " << sizes[i] << std::endl;
-        std::cout << "Tipo de vetor: Aleat" << char(162) << "rio" << std::endl;
-        std::cout << "M" << char(130) << "todo: " << methodsNames[method] << std::endl;
-        std::cout << "Resultado: " << sortVet(vet, sizes[i], method) << std::endl << std::endl;
-        free(vet);
-    }
-    */
 
     // PARA TESTAR TODOS OS MÉTODOS COM TODOS OS TAMANHOS DE TODOS OS TIPOS DE VETOR
     for (j = 0; j < AMOUNT_OF_METHODS; j++) {
